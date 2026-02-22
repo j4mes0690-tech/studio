d1f46f1efa45f2dbf23fa1d36655031b4e0d99b0
James HCC <j4mes0690@gmail.com>
2026-02-22 10:38:44 +0000

    no its still being stretched

--- src/app/information-requests/edit-information-request.tsx
+++ src/app/information-requests/edit-information-request.tsx
@@ -320,7 +320,7 @@ export function EditInformationRequest({ item, projects, distributionUsers }: Ed
                           </Button>
                         </FormControl>
                       </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
+                      <PopoverContent className="p-0" align="start">
                         <Calendar
                           mode="single"
                           selected={field.value ? new Date(field.value) : undefined}
--- src/app/information-requests/new-information-request.tsx
+++ src/app/information-requests/new-information-request.tsx
@@ -288,7 +288,7 @@ export function NewInformationRequest({ projects, distributionUsers }: NewInform
                           </Button>
                         </FormControl>
                       </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
+                      <PopoverContent className="p-0" align="start">
                         <Calendar
                           mode="single"
                           selected={field.value ? new Date(field.value) : undefined}
