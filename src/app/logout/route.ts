eeb184ec6142d6221227c3a74ea005694c772ef9
James HCC <j4mes0690@gmail.com>
2026-02-22 13:09:51 +0000

    everything i click on logs me out still

--- src/app/logout/route.ts
+++ src/app/logout/route.ts
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { type NextRequest } from 'next/server'
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   cookies().delete('userId');
   redirect('/login');
 }
