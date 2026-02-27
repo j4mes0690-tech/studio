5dc5f9ab8c8b47987385b6ab11b1647654760477
James HCC <j4mes0690@gmail.com>
2026-02-27 20:17:26 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-// This configuration is specific to your Firebase project.
-// You can find these values in the Firebase Console: 
-// Project Settings > General > Your apps > SDK setup and configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyBl6qhMKDP3hp3I6AnPvvAn7aymg9ORrmQ",
-  authDomain: "sitecommand2-93834253-fc971.firebaseapp.com",
-  projectId: "sitecommand2-93834253-fc971",
-  storageBucket: "sitecommand2-93834253-fc971.firebasestorage.app",
-  messagingSenderId: "214218502319",
-  appId: "1:214218502319:web:400fd94a2818e34bd0e45a"
+  "projectId": "sitecommand2-93834253-fc971",
+  "appId": "1:214218502319:web:400fd94a2818e34bd0e45a",
+  "apiKey": "AIzaSyBl6qhMKDP3hp3I6AnPvvAn7aymg9ORrmQ",
+  "authDomain": "sitecommand2-93834253-fc971.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "214218502319"
 };
