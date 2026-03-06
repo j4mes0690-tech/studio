3745b6d88be07bce883b1571cfa2a94cdd349b1e
James HCC <j4mes0690@gmail.com>
2026-03-06 15:40:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/settings/edit-project-form.tsx
+++ src/app/settings/edit-project-form.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useForm } from 'react-hook-form';
@@ -7,6 +6,7 @@ import { z } from 'zod';
 import { useTransition, useState, useEffect, useMemo } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import {
   Dialog,
